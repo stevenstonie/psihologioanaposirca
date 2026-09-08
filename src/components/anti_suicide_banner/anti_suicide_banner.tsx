b8660eeeed54e671893c5fa11e90e820b8adb877
stevenstonie <stevennstonie@gmail.com>
2026-09-08 13:58:25 +0300

    (update, feat) made anti-suicide banner do a marquee scroll; also added a loading animation to index.html, before the page loads

--- src/components/anti_suicide_banner/anti_suicide_banner.tsx
+++ src/components/anti_suicide_banner/anti_suicide_banner.tsx
@@ -2,7 +2,27 @@ import './anti_suicide_banner.scss';
 
 export default function AntiSuicideBanner() {
     return (
-        <div className="crisis-banner">
+        <div className="crisis-banner-viewport">
+            <div className="crisis-banner-track">
+                {/* primary group */}
+                <div className="crisis-group">
+                    <BannerItem />
+                    <BannerItem />
+                </div>
+
+                {/* duplicate group hidden from screen readers to prevent double reading */}
+                <div className="crisis-group" aria-hidden="true">
+                    <BannerItem />
+                    <BannerItem />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function BannerItem() {
+    return (
+        <div className="crisis-item">
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
@@ -20,7 +40,7 @@ export default function AntiSuicideBanner() {
                 <line x1="12" y1="9" x2="12" y2="13" />
                 <line x1="12" y1="17" x2="12.01" y2="17" />
             </svg>
-            <span className='crisis-text'>
+            <span className="crisis-text">
                 Dacă te afli într-o situație de urgență sau ai gânduri de a-ți face rău, sună la <a href="tel:112">112</a> sau la Telefonul Copilului <a href="tel:116111">116 111</a>, Asociația Antisuicid <a href="tel:0800801200">0800 801 200</a>.
             </span>
         </div>
